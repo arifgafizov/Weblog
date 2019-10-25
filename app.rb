23b571f5d0a3da6b7a27b2ddf8ee5b838ddb0fd1
Arif Gafizov <agafizov@gmail.com>
2019-10-25 12:50:33 +0300

    Init database and make @db variable and create table Posts

--- app.rb
+++ app.rb
@@ -1,6 +1,33 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
+def init_db
+	@db = SQLite3::Database.new 'weblog.db'
+	# возвращает результат как хеш а не массив
+	@db.results_as_hash = true 
+end
+
+# before вызывается каждый раз при перезагрузке
+# любой страницы
+
+before do
+	# индициализация БД
+	init_db
+end
+
+configure do
+	init_db
+	# создает таблицу если таблица не существует
+	@db.execute 'create table if not exists Posts
+	(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		created_date DATE,
+		content TEXT,
+		username TEXT
+	)'
+end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
